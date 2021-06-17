0e682a372af943edf9007c42ae04bd51b79177ea
fabcodingzest <fabrizvi786@gmail.com>
2021-06-17 23:21:50 +0530

    setting up form for sell

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,67 @@
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+
+export default function CreateProduct() {
+  const { inputs, handleChange, resetForm, clearForm } = useForm({
+    images: '',
+    name: 'Nice shoes',
+    price: 2321,
+    description: 'These are the best shoes',
+  });
+
+  return (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(inputs);
+      }}
+    >
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
+  );
+}
